acee6b0747fd5bdc7b01f3563ee75717861753f3
Amanda George <github email address>
2017-06-27 17:34:26 -0500

    add login games index spec

--- spec/application_controller_spec.rb
+++ spec/application_controller_spec.rb
@@ -79,5 +79,33 @@ describe ApplicationController do
     end
   end
 
+  it 'loads the game index after login' do
+      user = User.create(:username => "walle", :email => "wall@e.com", :password => "eve")
+      params = {
+        :username => "walle",
+        :password => "eve"
+      }
+      post '/login', params
+      expect(last_response.status).to eq(302)
+      follow_redirect!
+      expect(last_response.status).to eq(200)
+      expect(last_response.body).to include("Welcome,")
+    end
+
+    it 'does not let user view login page if already logged in' do
+      user = User.create(:username => "walle", :email => "wall@e.com", :password => "eve")
+
+      params = {
+        :username => "walle",
+        :password => "eve"
+      }
+      post '/login', params
+      session = {}
+      session[:user_id] = user.id
+      get '/login'
+      expect(last_response.location).to include("/games")
+    end
+  end
+
 
 end
