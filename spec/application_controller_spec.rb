c9a3bea9e1bc855dba3955e73717a56adb52dc24
Amanda George <github email address>
2017-06-27 17:30:00 -0500

    add signup required fields spec

--- spec/application_controller_spec.rb
+++ spec/application_controller_spec.rb
@@ -28,4 +28,48 @@ describe ApplicationController do
     end
   end
 
+    it 'does not let a user sign up without a username' do
+      params = {
+        :username => "",
+        :email => "pea@nut.com",
+        :password => "elephant"
+      }
+      post '/signup', params
+      expect(last_response.location).to include('/signup')
+    end
+
+    it 'does not let a user sign up without an email' do
+      params = {
+        :username => "peanut123",
+        :email => "",
+        :password => "elephant"
+      }
+      post '/signup', params
+      expect(last_response.location).to include('/signup')
+    end
+
+    it 'does not let a user sign up without a password' do
+      params = {
+        :username => "peanut123",
+        :email => "pea@nut.com",
+        :password => ""
+      }
+      post '/signup', params
+      expect(last_response.location).to include('/signup')
+    end
+
+    it 'does not let a logged in user view the signup page' do
+      user = User.create(:username => "peanut123", :email => "pea@nut.com", :password => "elephant")
+      params = {
+        :username => "peanut123",
+        :email => "pea@nut.com",
+        :password => "elephant"
+      }
+      post '/signup', params
+      session = {}
+      session[:user_id] = user.id
+      get '/signup'
+      expect(last_response.location).to include('/games')
+    end
+
 end
