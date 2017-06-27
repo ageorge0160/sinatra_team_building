f72dbd96a4a74f927f0a4de2e1e35c4d2cb17898
Amanda George <github email address>
2017-06-27 17:44:15 -0500

    controller get signup

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,3 +1,11 @@
 class UserController < ApplicationController
 
+  get '/signup' do
+      if logged_in?
+        redirect '/games'
+      else
+        erb :'/users/new_user'
+      end
+    end
+
 end
