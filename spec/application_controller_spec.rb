e73bc423081cfa3f643ba7d61ca84d7a265f9123
Amanda George <github email address>
2017-06-27 17:27:01 -0500

    add signup specs

--- spec/application_controller_spec.rb
+++ spec/application_controller_spec.rb
@@ -10,4 +10,22 @@ describe ApplicationController do
     end
   end
 
+  describe "Signup Page" do
+
+    it 'loads the signup page' do
+      get '/signup'
+      expect(last_response.status).to eq(200)
+    end
+
+    it 'signup directs user to game index' do
+      params = {
+        :username => "peanut123",
+        :email => "pea@nut.com",
+        :password => "elephant"
+      }
+      post '/signup', params
+      expect(last_response.location).to include("/games")
+    end
+  end
+
 end
