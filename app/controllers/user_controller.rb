a188abd0d2993bfe52d88be421d6c5c936a13b3e
Amanda George <github email address>
2017-06-27 21:32:29 -0500

    user controls

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -2,29 +2,48 @@ class UserController < ApplicationController
 
   get '/signup' do
       if logged_in?
-        redirect 'games'
+        redirect '/games'
       else
-        erb :'users/new_user'
+        erb :'/users/new_user'
       end
     end
 
   get '/login' do
     if logged_in?
-      redirect 'login'
+      redirect '/login'
     else
-      erb :'users/login'
+      erb :'/users/login'
     end
   end
 
   get '/logout' do
-      if logged_in?
-        session.clear
-        redirect 'login'
-      else
-        redirect ''
-      end
+    if logged_in?
+      session.clear
+      redirect '/login'
+    else
+      redirect ''
     end
+  end
 
+  post '/signup' do
+    if params["username"] != nil && params["email"] != nil && params["password"] != nil
+      @user = User.create(username: params("username"), email: params("email"), password: params("password"))
+      @user.save
+      session[:user_id] = @user.id
+      redirect '/games'
+    else
+      redirect '/signup'
+    end
+  end
 
+  post '/login' do
+    @user = User.find(username: params["username"])
+    if @user && @user.authenticate(params["password"])
+      session[:user_id] = @user.id
+      redirect "/games"
+    else
+      redirect ''
+    end
+  end
 
 end
