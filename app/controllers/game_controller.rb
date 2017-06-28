3cfa984ed54b9112063550ebc417af4aa52e467e
Amanda George <github email address>
2017-06-28 15:16:16 -0500

    create menu bar

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -27,6 +27,7 @@ class GameController < ApplicationController
 
   get '/games/:id/edit' do
     if logged_in?
+      @game = Game.find(params[:id])
       erb :'games/edit_game'
     else
       redirect ''
@@ -35,10 +36,30 @@ class GameController < ApplicationController
 
   post '/games/new' do
     game = Game.new(params)
-    game.user = current_user
+    game.user_id = current_user.id
     game.save
     redirect "/games/#{game.id}"
   end
+
+  patch '/games/:id' do
+    game = Game.find(params[:id])
+    if game.user_id = current_user.id
+      game.update(params)
+
+      # game.title = params("title")
+      # game.description = params("description")
+      # game.goal = params("goal")
+      # game.rules = params("rules")
+      # game.safety = params("safety")
+      # game.debrief_topic = params("debrief_topic")
+      # game.time = params("time")
+      # game.save
+      # redirect '/games/<%=game.id%>'
+    else
+      erb :'games/games_home'
+    end
+  end
+
 
 # Helper Methods
   helpers do
