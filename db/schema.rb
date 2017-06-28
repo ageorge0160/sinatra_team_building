7bd2dd58ce1857b2bbec5596e86d33200f3c4ae0
Amanda George <github email address>
2017-06-28 13:41:10 -0500

    add user id to games

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170627221726) do
+ActiveRecord::Schema.define(version: 20170628183925) do
 
   create_table "games", force: :cascade do |t|
     t.string "title"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20170627221726) do
     t.string "safety"
     t.string "debrief_topic"
     t.integer "time"
+    t.string "user_id"
   end
 
   create_table "users", force: :cascade do |t|
