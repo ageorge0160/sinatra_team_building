f8fa679e8c01b8289d4a449d8f4f05ac8c90541c
Amanda George <github email address>
2017-06-27 20:56:36 -0500

    add game files

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -27,41 +27,4 @@ class ApplicationController < Sinatra::Base
       end
     end
 
-
-
-# User Controller
-
-    get '/signup' do
-        if logged_in?
-          redirect 'games'
-        else
-          erb :'users/new_user'
-        end
-      end
-
-    get '/login' do
-      if logged_in?
-        redirect 'login'
-      else
-        erb :'users/login'
-      end
-    end
-
-    get '/logout' do
-        if logged_in?
-          session.clear
-          redirect 'login'
-        else
-          redirect ''
-        end
-      end
-
-
-# Game Controller
-
-get 'games' do
-  erb :'games/games_home'
-end
-
-
 end
--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -1,4 +1,10 @@
 class GameController < ApplicationController
 
 
+  get '/games' do
+    erb :'games/games_home'
+  end
+
+
+
 end
